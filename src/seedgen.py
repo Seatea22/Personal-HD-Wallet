966908d8fa3a21986bce9e4e0d1df112ea70d9e8
“Seatea22” <cjt7922@rit.edu>
2024-07-16 03:24:31 +0200

    Privatized functions and variables

--- src/seedgen.py
+++ src/seedgen.py
@@ -7,15 +7,32 @@ import pyperclip
 
 class SeedGen:
     def __init__(self, seed="", passphrase=None, iterations=100000):
-        self.seed = "mnemonic" + seed
-        self.passphrase = passphrase
-        self.iterations = iterations
+        self._seed = "mnemonic" + seed
+        self._passphrase = passphrase
+        self._iterations = iterations
+        self._final_hash_seed = ""
 
-    def generate_entropy(self, seed_length: int) -> str:
+    @property
+    def final_hash_seed(self) -> str:
+        return self._final_hash_seed
+
+    def generate_seed(self, seed_length=12) -> None:
+        if seed_length != 12 and seed_length != 24:
+            raise ValueError("Please provide a seed length of 12 or 24")
+
+        if self._passphrase is None or len(self._passphrase) == 0:
+            entropy_bits = self.__generate_entropy(seed_length)
+            mnemonic = self.__generate_checksum(entropy_bits)
+            self.__generate_phrase(mnemonic)
+
+        self._final_hash_seed = hashlib.pbkdf2_hmac('sha512', self._passphrase.encode('utf-8'),
+                                                    self._seed.encode('utf-8'), 2048).hex()
+
+    def __generate_entropy(self, seed_length: int) -> str:
         bit_length = 128 if seed_length == 12 else 256
         return bin(secrets.randbits(bit_length))[2:].zfill(bit_length)
 
-    def generate_checksum(self, entropy_bits: str, ):
+    def __generate_checksum(self, entropy_bits: str, ):
         sha256_hash = hashlib.sha256(
             int(entropy_bits, 2).to_bytes((len(entropy_bits) + 7) // 8, byteorder='big')).digest()
         sha256_bin_hash = ''.join(format(byte, '08b') for byte in sha256_hash)
@@ -25,34 +42,11 @@ class SeedGen:
 
         return entropy_bits + checksum
 
-    def read_words(self) -> List[str]:
+    def __read_words(self) -> List[str]:
         with open("src/wordlist.txt") as f:
             wordlist = [line.strip() for line in f]
         return wordlist
 
-    def generate_phrase(self, mnemonic: str) -> None:
-        wordlist = self.read_words()
-        self.passphrase = ' '.join([wordlist[int(mnemonic[i:i + 11], 2)] for i in range(0, len(mnemonic), 11)])
-
-    def generate_seed(self, seed_length=12):
-        if seed_length != 12 and seed_length != 24:
-            raise ValueError("Please provide a seed length of 12 or 24")
-
-        if self.passphrase is None or len(self.passphrase) == 0:
-            entropy_bits = self.generate_entropy(seed_length)
-
-            mnemonic = self.generate_checksum(entropy_bits)
-
-            self.generate_phrase(mnemonic)
-            pyperclip.copy(self.passphrase)
-
-        hash = hashlib.pbkdf2_hmac(
-            'sha512',
-            self.passphrase.encode('utf-8'),
-            self.seed.encode('utf-8'),
-            2048
-        )
-
-        hash_hex = hash.hex()
-        print(len(hash_hex), hash_hex)
-        print(self.passphrase, self.seed)
+    def __generate_phrase(self, mnemonic: str) -> None:
+        wordlist = self.__read_words()
+        self._passphrase = ' '.join([wordlist[int(mnemonic[i:i + 11], 2)] for i in range(0, len(mnemonic), 11)])
